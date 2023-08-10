9cd94d2ea81fe95e69e5b59cb3d8d30f87efad68
wygulmage <wygulmage@noreply.github.com>
2023-08-10 00:14:47 -0400

    cleaned up tests

--- bytestring-text-short/test/Bench.hs
+++ bytestring-text-short/test/Bench.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings
+           , BangPatterns
   #-}
 
 
@@ -10,51 +11,64 @@ import qualified Data.List as List
 
 main :: IO ()
 main = defaultMain $
-    (bgroup "search" $
-        (bench "first index elemIndices"
-            $ nf (List.null . elemIndicesBS 47) "/ascii ByteString") :
-        (bench "first index elemIndices (unprocessed)"
-            $ nf (\ (pat, txt) -> List.null (elemIndicesBS pat txt)) (47, "/ascii ByteString")) :
-        (bench "first index indicesBrutal"
-            $ nf (List.null . indicesBrutalBS "/") "/ascii ByteString") :
+    (let
+       !needle = "/"
+       !haystack = "/ascii ByteString"
+       !both = (needle, haystack)
+    in bgroup "first index" $
+        (bench "elemIndices"
+            $ nf (List.null . elemIndicesBS 47) haystack) :
+        (bench "elemIndices (unprocessed)"
+            $ nf (\ (pat, txt) -> List.null (elemIndicesBS pat txt)) (47, haystack)) :
+        (bench "indicesBrutal"
+            $ nf (List.null . indicesBrutalBS needle) haystack) :
         (bench "first index indicesBrutal (unprocessed)"
-            $ nf (\ (pat, txt) -> List.null (indicesBrutalBS pat txt)) ("/", "/ascii ByteString")) :
+            $ nf (\ (pat, txt) -> List.null (indicesBrutalBS pat txt)) both) :
         (bench "first index indicesTwoWay"
-            $ nf (List.null . indicesTwoWayBS "/") "/ascii ByteString") :
+            $ nf (List.null . indicesTwoWayBS needle) haystack) :
         (bench "first index indicesTwoWay (unprocessed)"
-            $ nf (\ (pat, txt) -> List.null (indicesTwoWayBS pat txt)) ("/", "/ascii ByteString")) :
+            $ nf (\ (pat, txt) -> List.null (indicesTwoWayBS pat txt)) both) :
         (bench "first index indices"
-            $ nf (List.null . indicesBS "/") "/ascii ByteString") :
+            $ nf (List.null . indicesBS needle) haystack) :
         (bench "first index indices (unprocessed)"
-            $ nf (\ (pat, txt) -> List.null (indicesBS pat txt)) ("/", "/ascii ByteString")) :
-
-        (bench "index elemIndices"
-            $ nf (List.null . elemIndicesBS 58) "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal") :
-        (bench "index indicesBrutal"
-            $ nf (List.null . indicesBrutalBS ":") "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal") :
-        (bench "index indicesBrutal (unprocessed)"
-            $ nf (\ (pat, txt) -> List.null (indicesBrutalBS pat txt)) (":", "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal")) :
-        (bench "index indicesTwoWay"
-            $ nf (List.null . indicesTwoWayBS ":") "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal") :
-        (bench "index indicesTwoWay (unprocessed)"
-            $ nf (\ (pat, txt) -> List.null (indicesTwoWayBS pat txt)) (":", "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal")) :
-        (bench "index indices"
-            $ nf (List.null . indicesBS ":") "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal") :
-        (bench "index indices (unprocessed)"
-            $ nf (\ (pat, txt) -> List.null (indicesBS pat txt)) (":", "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal")) :
-
-
-        (bench "short repetitive needle indicesBrutal"
-            $ nf (List.null . indicesBrutalBS "cccpcpc") "pcppccpucdceedecccpcpccpuputpccccp") :
-        (bench "short repetitive needle indicesTwoWay"
-            $ nf (List.null . indicesTwoWayBS "cccpcpc") "pcppccpucdceedecccpcpccpuputpccccp") :
-        (bench "short repetitive needle indicesTwoWay (unprocesed)"
-            $ nf (\ (pat, txt) -> List.null (indicesTwoWayBS pat txt)) ("cccpcpc", "pcppccpucdceedecccpcpccpuputpccccp")) :
-        (bench "short repetitive needle indices"
-            $ nf (List.null . indicesBS "cccpcpc") "pcppccpucdceedecccpcpccpuputpccccp") :
-        (bench "short repetitive needle indices (unprocesed)"
-            $ nf (\ (pat, txt) -> List.null (indicesBS pat txt)) ("cccpcpc", "pcppccpucdceedecccpcpccpuputpccccp")) :
-
-
+            $ nf (\ (pat, txt) -> List.null (indicesBS pat txt)) both) :
         []) :
+
+    (let
+      !needle = ":"
+      !haystack = "some.long.url.co.uk/webstore/foo.asp.net.php#:~:nothingHerePal"
+      !both = (needle, haystack)
+    in bgroup "index farther out" $
+      (bench "elemIndices"
+          $ nf (List.null . elemIndicesBS 58) haystack) :
+      (bench "indicesBrutal"
+          $ nf (List.null . indicesBrutalBS needle) haystack) :
+      (bench "indicesBrutal (unprocessed)"
+          $ nf (\ (pat, txt) -> List.null (indicesBrutalBS pat txt)) both) :
+      (bench "indicesTwoWay"
+          $ nf (List.null . indicesTwoWayBS needle) haystack) :
+      (bench "indicesTwoWay (unprocessed)"
+          $ nf (\ (pat, txt) -> List.null (indicesTwoWayBS pat txt)) both) :
+      (bench "index indices"
+          $ nf (List.null . indicesBS needle) haystack) :
+      (bench "index indices (unprocessed)"
+          $ nf (\ (pat, txt) -> List.null (indicesBS pat txt)) both) :
+      []) :
+
+    (let
+        !needle = "cccpcpc"
+        !haystack = "pcppccpucdceedecccpcpccpuputpccccp"
+    in bgroup "short repetitive needle" $
+      (bench "indicesBrutal"
+          $ nf (List.null . indicesBrutalBS needle) haystack) :
+      (bench "indicesTwoWay"
+          $ nf (List.null . indicesTwoWayBS needle) haystack) :
+      (bench "indicesTwoWay (unprocesed)"
+          $ nf (\ (pat, txt) -> List.null (indicesTwoWayBS pat txt)) (needle, haystack)) :
+      (bench "indices"
+          $ nf (List.null . indicesBS needle) haystack) :
+      (bench "indices (unprocesed)"
+          $ nf (\ (pat, txt) -> List.null (indicesBS pat txt)) (needle, haystack)) :
+      []) :
+
     []
